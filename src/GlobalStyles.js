fcd711376e13ea1709a66202323a8f0f40d04392
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-25 14:48:39 +0900

    Create a global style component. (Specify the initial value of css)

---
+++ src/GlobalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+
+    *{
+        box-sizing:border-box;
+    }
+
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size:18px;
+    }
+`;
+
+export default globalStyles;
